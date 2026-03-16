eca2452df4e18342fa368df6d342933ca8cbd4c8
Maddha Dheeraj <maddhadheeraj@gmail.com>
2026-03-17 01:13:38 +0530

    Base application setup

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   Palindrome Checker App");
+        System.out.println("=================================");
+
+        System.out.println("Application Version : 1.0");
+        System.out.println("Welcome to the Palindrome Checker Application");
+
+    }
+}
